48db5803db25c8722438633066cbd4a5bc96d470
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-30 17:49:00 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name , email){
+
+    let myRequest={
+        method:"POST",
+        header:{
+            "Content-Type":"application/json",
+            Accept:"applicaation/json",
+        },
+        body:JSON.stringify({
+            name:"mitchel",
+            email:"learnwithm@gamil.com",
+        })
+    }
+   return fetch('http://localhost:3000/users', myRequest)
+    .then(res=> res.json())
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.error("there's an error",error);
+    }
+    
+    )
+
+}
